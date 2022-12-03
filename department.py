eebfc378963c206af52a0aa9f4eeeed1953b7282
Зыков Михаил <m.zykov@corp.mail.ru>
2022-12-04 00:10:26 +0300

    ha9: at least 1 protected and 1 private variable at least 1 classmethod and 1 staticmethod at least 1 property with setter at least 1 descriptor

--- department.py
+++ department.py
@@ -1,35 +1,64 @@
+from position import Position
+
 class Department:
+    __positions = None
+    __employee_count = None
 
     def __init__(self, name, boss=None, parent=None):
-        self.name = name
-        self.employee_count = 0
-        self.positions = dict()
-        self.parent = parent
-        self.boss = boss
+        self._name = name
+        self._parent = parent
+        self._boss = boss
+        self.__positions = dict()
+        self.__employee_count = 0
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, new_name):
+        self._name = new_name
+
+    @property
+    def parent(self):
+        return self._parent
+
+    @parent.setter
+    def parent(self, p):
+        self._parent = p
+
+    @property
+    def boss(self):
+        return self._boss
+
+    @boss.setter
+    def boss(self, b):
+        self._boss = b
+
+    @property
+    def employee_count(self):
+        return self.__employee_count
 
     def update_employee_count(self, delta):
-        self.employee_count += int(delta)
-        return self.employee_count
+        self.__employee_count += int(delta)
 
     def add_position(self, name):
         position = Position(name, self)
-        self.positions[name] = position
+        self.__positions[name] = position
+        return position
 
     def add_positions(self, names):
         for name in names:
             self.add_position(name)
 
     def position(self, name):
-        if name in self.positions.keys():
-            return self.positions[name]
+        if name in self.__positions.keys():
+            return self.__positions[name]
         else:
             return None
 
-    def assign_boss(self, boss):
-        self.boss = boss
-
-    def set_parent(self, parent):
-        self.parent = parent
+    def vacant_positions(self):
+        return sorted(filter(lambda p: p.is_vacant, self.__positions.values()), key=lambda x: x.name)
 
     def __repr__(self, short=False):
         parts = [ self.name ]
